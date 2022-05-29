84cd73e2441aac883f625ed1fca23c415bf8a754
Nal Bilalova <59770118+bnfnal@users.noreply.github.com>
2022-05-29 21:13:53 +0300

    Update Main.java
    Task 2: A Rule of Divisibility by 7

--- Main.java
+++ Main.java
@@ -24,6 +24,20 @@ public class Main
         }
         System.out.println();
         System.out.println();
+        
+        // Task 2: A Rule of Divisibility by 7
+        
+        System.out.println("Task 2: A Rule of Divisibility by 7");
+        System.out.println("Введите натуральное число");
+        long n = sc.nextLong();
+        long[] c = DivSeven.seven(n);
+        System.out.println("Последнее число m и количество шагов:");
+        for (int i = 0; i < as.length; i++) {
+            System.out.print(c[i] + " ");
+        }
+        System.out.println();
+        System.out.println();
+  
     }
     
     // Task 1: Incrementer
@@ -40,6 +54,24 @@ public class Main
            return numbers;
        }
     }
-  
-}
+    
+    // Task 2: A Rule of Divisibility by 7
+    
+    public static class DivSeven 
+    {
+        public static long[] seven(long m) 
+        {
+            long[] res = new long[2];
+            long i = 0;
+            while(m > 99)
+            {
+                m = (m/10) - 2*(m%10);
+                i += 1;
+            }
+            res[0] = m;
+            res[1] = i;
+            return res;
+        }
+    }
 
+}
